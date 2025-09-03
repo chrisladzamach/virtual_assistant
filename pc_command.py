b3897481081d3898c30e6e25da7fc06cab984e41
OrichalcosShunnoros <c3146092091@gmail.com>
2025-09-02 19:21:06 -0500

    Actualización: Código base (de momento no funciona)

---
+++ pc_command.py
@@ -0,0 +1,13 @@
+from subprocess import call
+"""
+Clase para ejecutar comandos en la PC
+De momento esta en duro funcional para Windows hohoh
+"""
+class PcCommand():
+    def __init__(self):
+        pass
+    
+    def open_chrome(self, website):
+        website = "" if website is None else website
+        # Funciona para windows, si quiere para otro, modifiquelo.
+        call("C:/Program Files/Google/Chrome/Application/chrome.exe " + website)
